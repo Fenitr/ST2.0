ec668443bcc330cd6d16cace2f55710642cee666
TianaIrene <ralisonirene429@gmail.com>
2024-09-10 14:06:13 +0300

    Changement Index

--- STFront2/App.js
+++ STFront2/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Routes from './routes';
-
+import Buttonx from './bouton'
 const App = () => {
   return (
     <View style={styles.container}>
       <Routes />
+      <Buttonx />
       <StatusBar style="auto" />
     </View>
   );
